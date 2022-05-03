490627c001efe3fae1d17c5b1222cf3998dc37ec
Ja <Ja_Pan_Krzysztof@gmail.com>
2022-05-03 09:45:02 +0100

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!E:\Python\..Python_venv\instaling\Scripts\python
 from dotenv import load_dotenv
 from os import getenv
+from platform import release
 
 from instalingLogIn import InstalingLogIn
 # from InstalingSolving import InstalingSolving
@@ -8,7 +9,11 @@ from instalingLogIn import InstalingLogIn
 load_dotenv()
 
 if __name__ == '__main__':
-    InstalingLogIn(
-        username=getenv('_USERNAME'),
-        password=getenv('_PASSWORD')
-    )
+    if release() == '11':
+        print('Please change windows system to version 10 :)')
+
+    else:
+        InstalingLogIn(
+            username=getenv('_USERNAME'),
+            password=getenv('_PASSWORD')
+        )
